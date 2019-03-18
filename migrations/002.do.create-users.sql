6616f3ee793b772eb013c43f3836e23a939f0189
Irem Sen <secilreel@yahoo.com>
2019-03-18 14:18:58 -0400

    created tables

--- migrations/002.do.create-users.sql
+++ migrations/002.do.create-users.sql
@@ -2,7 +2,7 @@ CREATE TABLE meerkats_users (
   id SERIAL PRIMARY KEY,
   user_name TEXT NOT NULL UNIQUE,
   full_name TEXT NOT NULL,
-  password TEXT NOT NULL,
+  password TEXT NOT NULL
 );
 
 ALTER TABLE meerkats_events
--- migrations/003.do.create-friends.sql
+++ migrations/003.do.create-friends.sql
@@ -1,6 +1,6 @@
 CREATE TABLE meerkats_friends (
     user_id INTEGER
-        REFERENCES meerkats_users(id) ON DELETE SET NULL
+        REFERENCES meerkats_users(id) ON DELETE SET NULL,
     friends_id INTEGER
         REFERENCES meerkats_users(id) ON DELETE CASCADE
 );
